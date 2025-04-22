bb5cc16a62055a022bb5707ca9604142b663f0f8
小水滴 <69565707+client2233@users.noreply.github.com>
2025-04-22 15:12:06 +0800

    通知图标5s更新一次

--- utils/util.js
+++ utils/util.js
@@ -1094,6 +1094,29 @@ function msgSecCheck(content, scene = 3) {
   });
 }
 
+// ==================== 实时监听 ====================
+//所有data全都由https获得，使用定时轮询的方法来实时更新
+
+function fetchData(url){
+  wx.request({
+    url: url,
+    method: 'GET',
+    success: (res) => {
+      return res.data;
+    },
+    fail: (err) => {
+      console.error(err);
+    }
+  });
+}
+
+const startPolling = (func,delay)=> {
+  setInterval(() => {
+    func();
+    console.log('更新图标');
+  }, delay)
+}
+
 module.exports = {
   // 初始化
   init,
@@ -1143,7 +1166,11 @@ module.exports = {
   msgSecCheck,
   
   // 日志工具
-  logger
+  logger,
+
+  //实时同步
+  startPolling,
+  fetchData
 }
 
 /**
